4a4f6c39c75046bfe300442fd9df223541759de1
xiaoshuang <xiaoshuang@dcrays.cn>
2022-04-13 10:48:07 +0800

    更改路径报告生成路径

--- testcases/run.py
+++ testcases/run.py
@@ -4,4 +4,4 @@ import os
 
 if __name__ == '__main__':
     pytest.main()
-    os.system("allure generate ./temps -o ./reports --clean")
+    os.system("allure generate /var/lib/jenkins/workspace/auto-test（git）/temps -o /var/lib/jenkins/workspace/auto-test（git）/reports --clean")
